e12dc1eafb3d8a83f2d1e3adece747f52853c129
LukasD <lukas.dvarionis@gmail.com>
2022-09-28 14:02:51 +0300

    Added strings.txt test data Added solution to Main.java: Reading text file lines one by one Each line gets converted to char array Then numbers get extacted from char array Looping through list of numbers in string line and if criteria is met line gets printed

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,53 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.ArrayList;
+
 public class Main {
+    static void printLineWhenContainsLargerNumber(String line, ArrayList<String> numberArray, int number){
+        for( String stringNumber :numberArray)
+        {
+            int parsedNumber = Integer.parseInt(stringNumber);
+            if(parsedNumber >= number)
+            {
+                System.out.println(line);
+                break;
+            }
+        }
+    }
+    static ArrayList<String> extractNumbersFromString(char[] symbols)
+    {
+        ArrayList<String> numbersArray = new ArrayList<>();
+        StringBuilder digit = new StringBuilder() ;
+        for (int i = 0; i < symbols.length; i++)
+        {
+            if(Character.isDigit(symbols[i]))
+                digit.append(symbols[i]);
+            else if (!digit.toString().equals("")) {
+                numbersArray.add(digit.toString());
+                digit.setLength(0);
+            }
+        }
+        //adding last number
+        if (!digit.toString().equals("")) {
+            numbersArray.add(digit.toString());
+        }
+        return numbersArray;
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try {
+            File file = new File("strings.txt");
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNextLine()) {
+                String data = scanner.nextLine();
+                char[] symbols = data.toCharArray();
+                ArrayList<String> numbersArray = extractNumbersFromString(symbols);
+                printLineWhenContainsLargerNumber(data, numbersArray, 10);
+            }
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+            e.printStackTrace();
+        }
     }
 }
