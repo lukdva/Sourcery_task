9dcf018b983b642f369e04f5ffe7ea4e0ffffdbe
LukasD <lukas.dvarionis@gmail.com>
2022-10-11 11:15:23 +0300

    Refactored Main to make it more readable

--- src/Main.java
+++ src/Main.java
@@ -4,44 +4,45 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Main {
-    static void printLineWhenContainsLargerNumber(String line, ArrayList<String> numberArray, int number){
-        for( String stringNumber :numberArray)
+    static void printLineWhenContainsLargerNumber(String line, ArrayList<Integer> numberArray, int baseLineNumber){
+        for( Integer number :numberArray)
         {
-            int parsedNumber = Integer.parseInt(stringNumber);
-            if(parsedNumber >= number)
+            if(number >= baseLineNumber)
             {
                 System.out.println(line);
                 break;
             }
         }
     }
-    static ArrayList<String> extractNumbersFromString(char[] symbols)
+    static ArrayList<Integer> extractNumbersFromString(String line)
     {
-        ArrayList<String> numbersArray = new ArrayList<>();
-        StringBuilder digit = new StringBuilder() ;
+        char[] symbols = line.toCharArray();
+        ArrayList<Integer> numbersArray = new ArrayList<>();
+        StringBuilder number = new StringBuilder() ;
         for (int i = 0; i < symbols.length; i++)
         {
             if(Character.isDigit(symbols[i]))
-                digit.append(symbols[i]);
-            else if (!digit.toString().equals("")) {
-                numbersArray.add(digit.toString());
-                digit.setLength(0);
+                number.append(symbols[i]);
+            else if (!number.toString().equals("")) {
+                numbersArray.add(Integer.parseInt(number.toString()));
+                number.setLength(0);
             }
         }
         //adding last number
-        if (!digit.toString().equals("")) {
-            numbersArray.add(digit.toString());
+        if (!number.toString().equals("")) {
+            numbersArray.add(Integer.parseInt(number.toString()));
         }
+
         return numbersArray;
     }
     public static void main(String[] args) {
+
         try {
             File file = new File("strings.txt");
             Scanner scanner = new Scanner(file);
             while (scanner.hasNextLine()) {
                 String data = scanner.nextLine();
-                char[] symbols = data.toCharArray();
-                ArrayList<String> numbersArray = extractNumbersFromString(symbols);
+                ArrayList<Integer> numbersArray = extractNumbersFromString(data);
                 printLineWhenContainsLargerNumber(data, numbersArray, 10);
             }
             scanner.close();
